8ee87acebda4ef31356030d02c7944c614671a92
JeremieDrazic <jerem.drazic@gmail.com>
2016-03-04 15:38:53 +0100

    mobile and websockets ready

--- server/server.js
+++ server/server.js
@@ -3,12 +3,22 @@ let app  = require('express')();
 let http = require('http').Server(app);
 let io   = require('socket.io')(http);
 
-app.get('/', function(req, res) {
-  res.sendfile('index.html');
+app.get('/', function(req, res){
+  res.send('<h1>Hello world</h1>');
 });
 
 io.on('connection', function(socket) {
   console.log('a user connected');
+
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+
+  socket.on('acceleration', function(data){
+    // console.log(data);
+    io.emit('acceleration', data);
+  });
+
 });
 
 http.listen(3000, function() {
