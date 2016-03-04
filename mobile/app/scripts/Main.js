68c8b36aeb7047200a2d5bd3a6441e293965d910
JeremieDrazic <jerem.drazic@gmail.com>
2016-03-04 11:35:49 +0100

    updated server and mobile folders

---
+++ mobile/app/scripts/Main.js
@@ -0,0 +1,17 @@
+/**
+ * scripts/main.js
+ *
+ * This is a sample CommonJS module.
+ * Take a look at http://browserify.org/ for more info
+ */
+
+export default class Main {
+  constructor() {
+
+  }
+
+  beep() {
+      console.log('beep');
+      console.log('beepbeep');
+  }
+}
