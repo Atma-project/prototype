4ded96421beb9b0344789c3be27c39afa27ea91f
robin-dela <delaporte.ro@gmail.com>
2016-03-04 09:52:25 +0100

    first commit

---
+++ app/scripts/app.js
@@ -0,0 +1,14 @@
+/**
+ * scripts/main.js
+ *
+ * This is the starting point for your application.
+ * Take a look at http://browserify.org/ for more info
+ * See https://babeljs.io/docs/learn-es2015/ for info on ES6
+ */
+
+import App from './Main';
+import Const from './utils/Const';
+
+let app = new App();
+
+app.beep();
